05f07af404e680ffaea7731dad71ea6c6d0dfe3d
mabemeira <mabe@cora.com.br>
2023-05-15 07:33:15 -0300

    authenticate user

--- routers/auth.py
+++ routers/auth.py
@@ -6,6 +6,7 @@ from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -29,6 +30,14 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
                     create_user_request: CreateUserRequest):
@@ -44,3 +53,11 @@ async def create_user(db: db_dependency,
 
     db.add(create_user_model)
     db.commit()
+
+@router.post("/token")
+async def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                    db: db_dependency):
+    
+    if not authenticate_user(form_data.username, form_data.password, db):
+        return 'Failed Authentication'
+    return 'Successful Authentication'
