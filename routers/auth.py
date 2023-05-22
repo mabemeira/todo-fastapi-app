f936f19c01a7242f00d76b465ec901d8645ff3c3
mabemeira <mabe@cora.com.br>
2023-05-22 07:18:37 -0300

    adding user dependency to authenticate a to-do post

--- routers/auth.py
+++ routers/auth.py
@@ -1,12 +1,12 @@
 from datetime import timedelta, datetime
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from starlette import status
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
-from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 from dotenv import load_dotenv
@@ -62,17 +62,17 @@ def create_acess_token(username: str, user_id: int, expires_delta: timedelta):
 
 def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
 
         if username is None or user_id is None:
-            raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, 
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                                 detail='Could not validate user')   
         return {'username': username, 'id': user_id}
     
     except JWTError:
-        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, 
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                                 detail='Could not validate user') 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -97,7 +97,7 @@ async def login_user_for_access_token(form_data: Annotated[OAuth2PasswordRequest
     user = authenticate_user(form_data.username, form_data.password, db)
     
     if not user:
-        return HTTPException(status_code=HTTP_401_UNAUTHORIZED, 
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                                 detail='Could not validate user') 
     
     token = create_acess_token(user.username, user.id, timedelta(minutes=20))
--- routers/todos.py
+++ routers/todos.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, Path
+from starlette import status
 from models import Todos
 from database import SessionLocal
-from starlette import status
 from pydantic import BaseModel, Field
+from .auth import get_current_user
 
 router = APIRouter()
 
@@ -16,6 +17,7 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
@@ -38,8 +40,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.dict())
+async def create_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    todo_model = Todos(**todo_request.dict(), owner_id=user.get('id'))
 
     db.add(todo_model)
     db.commit()
